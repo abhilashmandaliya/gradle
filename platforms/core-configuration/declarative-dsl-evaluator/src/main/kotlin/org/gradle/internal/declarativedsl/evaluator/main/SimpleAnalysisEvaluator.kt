7e64155fb5f0a18f74874cf3bd806a0c36128ab1
József Bartók <jbartok@gradle.com>
2025-02-25 14:32:54 +0200

    Add test to confirm problems

--- platforms/core-configuration/declarative-dsl-evaluator/src/main/kotlin/org/gradle/internal/declarativedsl/evaluator/main/SimpleAnalysisEvaluator.kt
+++ platforms/core-configuration/declarative-dsl-evaluator/src/main/kotlin/org/gradle/internal/declarativedsl/evaluator/main/SimpleAnalysisEvaluator.kt
@@ -42,6 +42,7 @@ class SimpleAnalysisEvaluator(
     val schemaBuilder: (DeclarativeScriptContext) -> InterpretationSchemaBuildingResult
 ) {
     companion object {
+        @JvmStatic // TODO: necessary?
         fun withSchema(settings: InterpretationSequence, project: InterpretationSequence): SimpleAnalysisEvaluator =
             SimpleAnalysisEvaluator {
                 when (it) {
